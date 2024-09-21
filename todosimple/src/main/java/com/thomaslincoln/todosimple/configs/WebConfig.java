5258b4fd6a5ebe0793d3733232923b84a1d375be
Thomas Lincoln <thmas5952@gmail.com>
2024-09-21 14:09:44 -0300

    feat: :sparkles: create simple frontend

--- todosimple/src/main/java/com/thomaslincoln/todosimple/configs/WebConfig.java
+++ todosimple/src/main/java/com/thomaslincoln/todosimple/configs/WebConfig.java
@@ -7,9 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 
-  public void addCorsMappings(CorsRegistry registry){
-    registry.addMapping("/**").allowedMethods(null);
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("GET");
   }
 }
