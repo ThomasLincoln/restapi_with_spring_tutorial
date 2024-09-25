5861eaa027494b3102cc57e7540bbfd2e6b3e132
Thomas Lincoln <thmas5952@gmail.com>
2024-09-25 16:15:03 -0300

    feat: :sparkles: Create User Details Service Implementation and JwtUtils

---
+++ todosimple/src/main/java/com/thomaslincoln/todosimple/services/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+// O principal objetivo dessa classe em uma aplicação Spring Security é fornecer uma ponte entre a sua base de dados e o 
+// sistema de autenticação do Spring Security. Quando alguém tenta fazer login, o Spring Security precisa de uma forma de
+//  carregar os dados do usuário para verificar as credenciais e determinar os privilégios (roles/perfis) do usuário.
+
+package com.thomaslincoln.todosimple.services;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.thomaslincoln.todosimple.models.User;
+import com.thomaslincoln.todosimple.repositories.UserRepository;
+import com.thomaslincoln.todosimple.security.UserSprintSecurity;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = this.userRepository.findyByUsername(username);
+    if(Objects.isNull(user)){
+      throw new UsernameNotFoundException(username);
+    }
+    return new UserSprintSecurity(user.getId(), user.getUsername(), user.getPassword(), user.getProfiles());
+  }
+
+}
